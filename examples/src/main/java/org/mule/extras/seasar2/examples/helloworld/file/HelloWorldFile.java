20c616781c68378774cbee672707cb0de8e864eb
saito <saito@d4618e8a-e43e-0410-8b93-8be3632ad3b9>
2008-02-18 02:24:51 +0000

    made a copy

---
+++ examples/src/main/java/org/mule/extras/seasar2/examples/helloworld/file/HelloWorldFile.java
@@ -0,0 +1,35 @@
+package org.mule.extras.seasar2.examples.helloworld.file;
+
+import org.mule.extras.seasar2.sender.S2MuleSender;
+import org.seasar.framework.container.S2Container;
+import org.seasar.framework.container.factory.S2ContainerFactory;
+import org.seasar.framework.exception.ResourceNotFoundRuntimeException;
+
+public class HelloWorldFile {
+	
+	// dicon ファイル
+	private static final String CONFIGURE_PATH = "helloworld-file.dicon";
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			// dicon ファイルを指定して S2 コンテナを生成する
+			S2Container container = S2ContainerFactory.create(CONFIGURE_PATH);
+			
+			// S2MuleSender のインスタンスを取得する
+			S2MuleSender sender = (S2MuleSender) container.getComponent(S2MuleSender.class);
+			
+			// メッセージを送信する
+			sender.dispatch("Hello World!");
+			
+			// 成功
+			System.out.println("The message is sent successfully.");
+			
+		} catch (ResourceNotFoundRuntimeException e){
+			System.out.println("dicon ファイルが見つかりません： " + CONFIGURE_PATH);
+		}
+	}
+	
+}
---
+++ examples/src/main/java/org/mule/extras/seasar2/examples/helloworld/soap/axis/HelloWorldSOAP.java
@@ -0,0 +1,32 @@
+package org.mule.extras.seasar2.examples.helloworld.soap.axis;
+
+import org.seasar.framework.container.S2Container;
+import org.seasar.framework.container.factory.S2ContainerFactory;
+import org.seasar.framework.exception.ResourceNotFoundRuntimeException;
+
+public class HelloWorldSOAP {
+	
+	// dicon ファイル
+	private static final String CONFIGURE_PATH = "helloworld-soap-axis.dicon";
+
+	public static void main(String[] args) {
+		try {
+			// dicon ファイルを指定して S2 コンテナを生成する
+			S2Container container = S2ContainerFactory.create(CONFIGURE_PATH);
+		
+			// S2MuleSender のインスタンスを取得する
+			Echo echo = (Echo) container.getComponent(Echo.class);
+		
+			// メッセージを送信する
+			String rtn = echo.echo("Hello World!");
+		
+			// 成功
+			System.out.println("The message is sent successfully.");
+			System.out.println("The Returned message is: \"" + rtn + "\"");
+		
+		} catch (ResourceNotFoundRuntimeException e){
+			System.out.println("dicon ファイルが見つかりません： " + CONFIGURE_PATH);
+		}
+	}
+
+}
