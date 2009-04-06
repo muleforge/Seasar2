9205a9d03dc4c453bef234f2665dda7ba9b48077
saito <saito@d4618e8a-e43e-0410-8b93-8be3632ad3b9>
2009-04-06 07:35:40 +0000

    プロジェクトファイルを奇麗にした

---
+++ mule-module-seasar2-functional-test/src/main/java/org/mule/extras/seasar2/ws/hello/Hello.java
@@ -0,0 +1,60 @@
+
+package org.mule.extras.seasar2.ws.hello;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for hello complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="hello">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="src" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "hello", propOrder = {
+    "src"
+})
+public class Hello {
+
+    protected String src;
+
+    /**
+     * Gets the value of the src property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getSrc() {
+        return src;
+    }
+
+    /**
+     * Sets the value of the src property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setSrc(String value) {
+        this.src = value;
+    }
+
+}
---
+++ mule-module-seasar2-functional-test/src/main/java/org/mule/extras/seasar2/ws/hello/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://cxf.receiver.seasar2.extras.mule.org/")
+package org.mule.extras.seasar2.ws.hello;
